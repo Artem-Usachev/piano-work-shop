cd1691c419b6147862f310f47aeb9fe70b47ca50
Artem-Usachev <77751845+Artem-Usachev@users.noreply.github.com>
2021-06-05 18:44:12 +0500

    mailer

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,31 +1,16 @@
 import { Popup } from "./Popup.js";
 export class PopupWithForm extends Popup {
-    constructor({ popup, submit }) {
+    constructor({ popup }) {
         super(popup);
-        this.submit = submit;
         this.submitForm = this.popup.querySelector('.popup__content')
         this.submitButton = this.submitForm.querySelector('.popup__button')
 
-    }
-    _getInputValues() {
-        const inputs = this.popup.querySelectorAll(`input`);
-        let values = {};
-        inputs.forEach((i) => {
-            values[i.name] = i.value;
-        });
-        return values;
     }
     _reset() {
         const formElement = this.popup.querySelector(`form`);
         formElement.reset();
     }
-    _submit() {
-        const values = this._getInputValues();
-        this.submit(values);
-    }
-    _transferSubmit() {
-        this._submit()
-    }
+
     renderLoading(loading) {
         if (loading) {
             this.submitButton.textContent = 'Сохранение...';
@@ -37,7 +22,7 @@ export class PopupWithForm extends Popup {
         super.setEventListeners();
         this.submitForm.addEventListener('submit', (e) => {
             e.preventDefault();
-            this._transferSubmit()
+            this.close()
         });
     }
     close() {
---
+++ mail.php
@@ -0,0 +1,38 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$service = $_POST['service'];
+$phone = $_POST['phone'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'etud.server@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'KEQ-tqA-2Mw-key'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 25; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('etud.server@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('masterskayia-etud@yandex.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку,<br>Телефон: ' .$phone. '<br>Желаемая услуга: ' .$service;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+}
+?>
